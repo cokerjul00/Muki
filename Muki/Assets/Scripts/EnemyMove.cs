edd1d4e004478337128a947ff584df6077cc447c
cokerjul00 <45615733+cokerjul00@users.noreply.github.com>
2022-11-28 09:48:13 -0800

    Initial project upload

---
+++ Muki/Assets/Scripts/EnemyMove.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyMove : MonoBehaviour
+{
+    NavMeshAgent Enemy;
+    GameObject player;
+    private bool inRadius = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Enemy = GetComponent<NavMeshAgent>();
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (inRadius == true)
+        {
+            Enemy.SetDestination(player.transform.position);
+        }
+        if (inRadius == false)
+        {
+            Enemy.SetDestination(Enemy.transform.position);
+        }
+    }
+    public void OnTriggerEnter(Collider collision)
+    {
+        inRadius = true;
+            
+    }
+
+    public void OnTriggerExit(Collider collision)
+    {
+        inRadius = false;
+
+    }
+
+}
