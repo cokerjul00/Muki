2148dd4fda52978ecd43fbfa6420988faf469f8b
cokerjul00 <45615733+cokerjul00@users.noreply.github.com>
2022-12-15 10:21:37 -0800

    save

--- Muki/Assets/Scripts/WingGameScript.cs
+++ Muki/Assets/Scripts/WingGameScript.cs
@@ -6,7 +6,7 @@ public class WingGameScript : MonoBehaviour
     public void Win()
     {
 
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 3);s
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 3);
         Cursor.visible = true;
         Cursor.lockState = CursorLockMode.None;
     }
